5a2e56cc7fce920ff7d69a6b0f43abfc686da6bd
namvv-bhsoft <namvv.bhsoft@gmail.com>
2023-06-08 17:55:04 +0700

    feat(view): setup

--- app/interfaces/http/rest/api_v1/api.py
+++ app/interfaces/http/rest/api_v1/api.py
@@ -3,4 +3,4 @@ from app.interfaces.http.rest.api_v1.endpoints import auth
 
 
 api_router = APIRouter()
-# api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
